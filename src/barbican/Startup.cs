595043c8b3903c89644b51f62183427175b48799
Robert Stiff <uatecuk@gmail.com>
2018-01-25 22:57:36 +0000

    Marginally functional websocket proxy

--- src/barbican/Startup.cs
+++ src/barbican/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Net.WebSockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -41,12 +43,14 @@ namespace barbican
             app.UseWebSockets();
             app.Use(async (context, next) =>
             {
-                if (context.Request.Path == "/ws")
+                if (context.Request.Path.StartsWithSegments("/proxy") )
                 {
+                    string[] pathParts = context.Request.Path.Value.Split('/');
+
                     if (context.WebSockets.IsWebSocketRequest)
                     {
                         WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        await Echo(context, webSocket);
+                        await Proxy(pathParts[2], Int32.Parse(pathParts[3]), webSocket);
                     }
                     else
                     {
@@ -59,7 +63,53 @@ namespace barbican
                 }
             });
         }
-            
+
+        private async Task Proxy(string hostname, int port, WebSocket webSocket)
+        {
+            using (TcpClient tcpClient = new TcpClient()) 
+            {
+                tcpClient.Connect(hostname, port);
+
+                Socket socket = tcpClient.Client;
+
+                CancellationTokenSource cancellationSource = new CancellationTokenSource();
+                Task sendTask = Task.Run(async () => {
+                    var buffer = new byte[1024 * 4];
+                    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    while (!result.CloseStatus.HasValue)
+                    {
+                        socket.Send(buffer);
+                        Console.WriteLine($"< {Encoding.UTF8.GetString(buffer, 0, result.Count)} |");
+                        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationSource.Token);
+                    } 
+                    cancellationSource.Cancel();
+                });
+
+                Task receiveTask = Task.Run(async () => {
+                    var buffer = new byte[1024 * 4];
+                    while (socket.Connected) 
+                    {
+                        int i = socket.Receive(buffer); 
+
+                        if ( i == 0 ) 
+                        {
+                            break;
+                        }
+                        string text = Encoding.UTF8.GetString(buffer, 0, i);
+                        Console.WriteLine($"> {text} |");
+                        // todo change this to binary when we are happy what it means, and if we can derive end of message
+                        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, i), WebSocketMessageType.Text, i != buffer.Length, cancellationSource.Token);
+                    }
+                    cancellationSource.Cancel();
+                });
+
+                
+                await sendTask;
+                await receiveTask;
+                socket.Close();
+            }
+        }
+
         private async Task Echo(HttpContext context, WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
@@ -67,6 +117,20 @@ namespace barbican
             while (!result.CloseStatus.HasValue)
             {
                 await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+        
+        private async Task Say(string message, HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)), result.MessageType, result.EndOfMessage, CancellationToken.None);
 
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
