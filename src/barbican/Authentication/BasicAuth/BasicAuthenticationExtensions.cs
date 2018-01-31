d95c2292f71b4b5441e15c33fc25287ed05f55dd
Robert Stiff <uatecuk@gmail.com>
2018-01-31 21:59:45 +0000

    Autogenerate basic auth password if no configuration set up.

--- src/barbican/Authentication/BasicAuth/BasicAuthenticationExtensions.cs
+++ src/barbican/Authentication/BasicAuth/BasicAuthenticationExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +13,21 @@ namespace barbican.Authentication.BasicAuth
         {
             var basicAuthOptions = new BasicAuthOptions();
             configuration.Bind(basicAuthOptions);
+
+            if ( string.IsNullOrEmpty(basicAuthOptions.Username) && string.IsNullOrEmpty(basicAuthOptions.Password) )
+            {
+                byte[] data = new byte[20];
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+                rng.GetBytes(data);
+                basicAuthOptions.Username = Convert.ToBase64String(data);
+
+                rng.GetBytes(data);
+                basicAuthOptions.Password = Convert.ToBase64String(data);
+
+                Console.WriteLine($"Using Basic Auth - Username: \"{basicAuthOptions.Username}\" Password: \"{basicAuthOptions.Password}\"");
+            }
+
             services.AddSingleton(basicAuthOptions);
         }
 
